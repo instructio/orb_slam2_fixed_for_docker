3d7e164dcead2d52078bec36402368b9e283d574
instructio <artpesh2021@yandex.ru>
2023-10-30 13:01:54 +0300

    add all

---
+++ build.sh
@@ -0,0 +1,22 @@
+
+echo "Configuring and building ORB_SLAM2 ..."
+
+mkdir build
+cd build
+cmake .. \
+  -DROS_BUILD_TYPE=Release \
+  -DPYTHON_EXECUTABLE:FILEPATH=/usr/bin/python3 \
+  -DCMAKE_CXX_STANDARD_LIBRARIES="-lboost_system"
+
+make -j
+make install
+
+cp ../lib/libDBoW2.so /usr/local/lib
+cp ../Thirdparty/g2o/lib/libg2o.so /usr/local/lib
+
+
+echo "Uncompress vocabulary ..."
+
+cd ../Vocabulary
+tar -xf ORBvoc.txt.tar.gz
+cd ..
